4da010111083e8d58b9042fd6df28d5e3d0a2583
Nick Klein <github@nickklein.nl>
2014-01-24 21:08:29 +0100

    lol
    docs

--- src/vk/simulation/Randomizer.java
+++ src/vk/simulation/Randomizer.java
@@ -10,7 +10,7 @@ import java.util.Random;
 public class Randomizer
 {
     // The default seed for control of randomization.
-    private static final int SEED = 1111;
+    private static int SEED = 1111;
     // A shared Random object, if required.
     private static final Random rand = new Random(SEED);
     // Determine whether a shared random generator is to be provided.
@@ -21,6 +21,7 @@ public class Randomizer
      */
     public Randomizer()
     {
+    	
     }
 
     /**
