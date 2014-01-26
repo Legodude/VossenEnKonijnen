49270c8983c41f91cfdf8fa634f4a3dd143d27f1
noyouh <jaacobsmark@gmail.com>
2014-01-26 01:41:47 +0100

    meer nerfs
    kut rabbits

--- src/vk/actor/Hunter.java
+++ src/vk/actor/Hunter.java
@@ -21,7 +21,7 @@ public class Hunter extends Animal {
     private static final int MAX_LITTER_SIZE = 5;
     // The food value of a single rabbit. In effect, this is the
     // number of steps a fox can go before it has to eat again.
-    private static final int RABBIT_FOOD_VALUE = 7;
+    private static final int RABBIT_FOOD_VALUE = 10;
     // A shared random number generator to control breeding.
     private static final Random rand = Randomizer.getRandom();
 	private static final int FOX_FOOD_VALUE = 5;
