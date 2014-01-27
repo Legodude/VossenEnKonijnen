d92f339037c6afce744e88771f2908aaa55b4f3d
Nick Klein <github@nickklein.nl>
2014-01-27 23:54:50 +0100

    misschien handig als ik dan ook de helpframe meecommit. eclipse git is nog wat wennen :P

---
+++ src/vk/view/menu/HelpItem.java
@@ -0,0 +1,15 @@
+package vk.view.menu;
+
+import javax.swing.JMenuItem;
+
+public class HelpItem extends JMenuItem {
+	/**
+	 * another one bites the dust
+	 */
+	private static final long serialVersionUID = -6996064757566770578L;
+
+	public HelpItem()
+	{
+		super("<HTML><U>H</U>elp</HTML>");
+	}
+}
---
+++ src/vk/view/windows/HelpFrame.java
@@ -0,0 +1,17 @@
+package vk.view.windows;
+
+import javax.swing.*;
+
+public class HelpFrame extends JDialog {
+
+	/**
+	 * nog eens
+	 */
+	private static final long serialVersionUID = -8839475536465368087L;
+
+	public HelpFrame()
+	{
+		createRootPane();
+		dialogInit();
+	}
+}
