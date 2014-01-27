e40f7ddd9fa7b6b6b5a744e9645f72255a95f090
Nick Klein <github@nickklein.nl>
2014-01-27 23:54:02 +0100

    laatste commit voor vandaag, helpframe geimplementeerd, alleen nog even vullôn

--- src/vk/view/menu/FileTab.java
+++ src/vk/view/menu/FileTab.java
@@ -1,9 +1,14 @@
 package vk.view.menu;
 
-import javax.swing.JMenu;
-import vk.view.menu.item.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class FileTab extends JMenu {
+import javax.swing.JMenu;
+
+import vk.view.menu.item.*;
+import vk.view.windows.HelpFrame;
+
+public class FileTab extends JMenu implements ActionListener {
 
 	/**
 	 * random nummertje
@@ -13,6 +18,30 @@ public class FileTab extends JMenu {
 	public FileTab()
 	{
 		super("<HTML><U>F</U>ile</HTML>");
-		this.add(new SettingsItem());
+		SettingsItem settingsItem = new SettingsItem();
+		this.add(settingsItem);
+		settingsItem.setActionCommand("settingsItem");
+        settingsItem.addActionListener(this);
+        
+        HelpItem helpItem = new HelpItem();
+		this.add(helpItem);
+		helpItem.setActionCommand("helpItem");
+        helpItem.addActionListener(this);
+        
 	}
+	
+	public void actionPerformed(ActionEvent event) {
+		System.out.println(event.getActionCommand());
+		if(event.getActionCommand().equals("settingsItem"))
+		{
+			
+		}
+		if(event.getActionCommand().equals("helpItem"))
+		{
+			HelpFrame helpFrame = new HelpFrame();
+			helpFrame.show();
+		}
+		
+	}
+	
 }
