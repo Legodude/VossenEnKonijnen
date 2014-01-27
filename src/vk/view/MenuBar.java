1c21c415dcbda4423c9b93bd0fc47c3d0de5fe3d
GaZzNL <robert-stocker@hotmail.com>
2014-01-27 12:15:28 +0100

    MenuBar
    checkcheck

--- src/vk/view/MenuBar.java
+++ src/vk/view/MenuBar.java
@@ -1,11 +1,20 @@
 package vk.view;
 
 import javax.swing.*;
+
 import java.awt.*;
 
 public class MenuBar extends JFrame {
 
+	public MenuBar()
+	{
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		JMenu fileMenu = new JMenu("File");
+		menuBar.add(fileMenu);
 	
+	
+	}
 	
 	
 }
